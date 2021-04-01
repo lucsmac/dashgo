0127f2fb49587d1ad60ac89c46a37a1a5c74b53b
Lucas Macedo <lucasmdc57@gmail.com>
2021-04-01 12:10:03 -0300

    adding elements to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
   return (
@@ -48,6 +48,44 @@ export function Header() {
         />
 
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+      
+      <Flex
+        align="center"
+        ml="auto"
+      >
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          boderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex
+          align="center"
+        >
+          <Box mr="4" textAlign="right">
+            <Text>Lucas Macedo</Text>
+            <Text
+              color="gray.300"
+              fontSize="small"
+            >
+              lucasmdc57@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar
+            size="md"
+            name="Lucas Macedo"
+            src="https://github.com/lucsmac.png"
+          />
+        </Flex>
       </Flex>
     </Flex>
   )
