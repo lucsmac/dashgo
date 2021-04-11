009fe739c9854b4bc98d9c7fe7fad5d415fb129e
Lucas Macedo <lucasmdc57@gmail.com>
2021-04-10 21:03:57 -0300

    implements pagination component logic

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -107,7 +107,11 @@ export default function UserList() {
                   </Tbody>
                 </Table>
 
-                <Pagination />
+                <Pagination 
+                  totalCountOfRegisters={200}
+                  currentPage={4}
+                  onPageChange={() => {}}
+                />
               </>
             )
           }
